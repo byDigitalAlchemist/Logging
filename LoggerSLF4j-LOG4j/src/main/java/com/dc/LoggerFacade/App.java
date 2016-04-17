23e37c9f68210dbc3b00bb227842d035e0498ad2
Digital Alchemist <developer.anwar@gmail.com>
2016-04-16 21:21:59 -0400

    Logging Profile, Properties and POM
    1- Logging enabled with SLF4J and LOG4J
    2- Properties File Added
    3- POM Updated with SLF4J and LOG4j

--- LoggerSLF4j-LOG4j/src/main/java/com/dc/LoggerFacade/App.java
+++ LoggerSLF4j-LOG4j/src/main/java/com/dc/LoggerFacade/App.java
@@ -1,13 +1,22 @@
 package com.dc.LoggerFacade;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+    
+    static Logger LOGGER = LoggerFactory.getLogger(App.class);
+    
+    public static void main(String[] args) {
+        for (int i = 0; i < 10; i++)
+            if (i % 2 == 0)
+                LOGGER.info("Hello {}", i);
+            else
+                LOGGER.debug("I am on index {}", i);
     }
 }
