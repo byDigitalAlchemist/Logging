f883c715d54b97654a905b8186b09ffb8dd25fce
Digital Alchemist <developer.anwar@gmail.com>
2016-04-16 21:45:05 -0400

    Different File for each logging level
    1- Level based different file logging
    2- Project Name renamed from MultiLevelLogging

--- MultiLevelLogging/src/main/java/com/dc/multilevellogging/App.java
+++ MultiLevelLogging/src/main/java/com/dc/multilevellogging/App.java
@@ -1,13 +1,21 @@
 package com.dc.multilevellogging;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+
 /**
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+public class App {
+	static Logger LOGGER = LoggerFactory.getLogger(App.class);
+
+	public static void main(String[] args) {
+		for (int i = 0; i < 10; i++)
+			if (i % 2 == 0)
+				LOGGER.info("Hello {}", i);
+			else
+				LOGGER.debug("I am on index {}", i);
+	}
 }
